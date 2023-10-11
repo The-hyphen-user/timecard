2923b6ef3d244bd5735e3e15101b011b15c77219
The-hyphen-user <pet22v20@gmail.com>
2023-10-11 12:46:55 -0700

    adding jotai shell

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { Provider } from 'jotai';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+  <Provider>
     <App />
+    </Provider>
   </React.StrictMode>
 );
 
