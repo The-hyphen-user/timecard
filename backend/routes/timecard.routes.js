f542f047a35c7c4ef8d39e11ccd7f30bf23fe7e7
daniel w <pet22v20@gmail.com>
2023-10-25 08:49:16 -0700

    added recent and search routes for timecard

--- backend/routes/timecard.routes.js
+++ backend/routes/timecard.routes.js
@@ -3,13 +3,40 @@ const router = express.Router();
 import Timecard from "../models/timecard.js";
 import isAuthenticated from '../util/isAuthenticated.js'
 //route: /api/timecard
+
+router.get('/recent', isAuthenticated, async (req, res) => {
+    try {
+        const timecards = await Timecard.find({ user: req.user._id }).sort({ createdAt: -1 }).limit(5)
+        res.json(timecards)
+    } catch (error) {
+        console.log(error)
+    }
+
+})
+
+router.get('/search', isAuthenticated, async (req, res) => {
+    try {
+        const params = req.query
+        const user = req.user._id
+        const searchTerm = params.searchTerm
+        const startDate = params.startDate
+        const endDate = params.endDate
+        const page = params.page
+        const results = await Timecard.find({})
+    } catch (error) {
+        console.log(error)
+    }
+})
+
 router.post('/', isAuthenticated, async (req, res) => {
     try {
         const timecard = new Timecard(req.body)
         timecard.user = req.user._id
         await timecard.save()
-        res.json({message: 'timecard submited', timecard: timecard})
-    } catch (error) { console.log(error) }
+        res.json({ message: 'timecard submited', timecard: timecard })
+    } catch (error) {
+        console.log(error)
+    }
 })
 
 router.get("/", isAuthenticated, async (req, res) => {
