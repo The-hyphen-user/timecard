e19e9a8c58ffd2fde80a16accabcc004fa93984a
daniel w <pet22v20@gmail.com>
2023-11-02 11:09:16 -0700

    Improved activation handling, covering edge cases

--- backend/routes/activation.routes.js
+++ backend/routes/activation.routes.js
@@ -10,21 +10,32 @@ router.post('/create', async (req, res) => {
         let newToken;
         let tokenExists = true;
 
+        const email = req.body.email
+        if (!email) {
+            res.status(404).json({ message: 'missing email' })
+        }
+        const dupUser = await User.findOne({ email: email })
+        if (dupUser) {
+            res.status(409).json({ message: "email already taken" })
+        }
+
         while (tokenExists) {
-            newToken = Token(16);
+            newToken = Token(32);
             const existingActivation = await UserActivation.findOne({ activationKey: newToken });
 
             if (!existingActivation) {
                 tokenExists = false;
+
             }
-            const userActivation = new UserActivation({ email: req.body.email, activationKey: newToken })
-            await userActivation.save()
-            console.log('userActivation:', userActivation)
-            res.status(201).json({ message: 'User created', activationKey: userActivation.activationKey })
         }
+        const userActivation = new UserActivation({ email: email, activationKey: newToken })
+        await userActivation.save()
+        console.log('userActivation:', userActivation)
+        res.status(201).json({ message: 'User created', activationKey: userActivation.activationKey })
     }
     catch (error) {
         console.log(error)
+        res.status(500).json({ message: 'Internal Server Error at activation key creation' });
     }
 })
 
