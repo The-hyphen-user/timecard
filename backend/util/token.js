6df79bdb6ee8f8a808a0b1ec09d0c644c28b4720
daniel w <pet22v20@gmail.com>
2023-11-02 11:07:29 -0700

    cleanup

--- backend/util/token.js
+++ backend/util/token.js
@@ -1,22 +1,8 @@
-//random token generator import
-
-
-// const crypto = require('crypto');
 import crypto from 'crypto';
 
-// function token(length) {
-//   return crypto.randomBytes(length).toString('hex');
-// }
 const token = (length) => {
     return crypto.randomBytes(length).toString('hex');
 }
 
-// module.exports = token;
-
-// const token = () => {
-//     //random token generator here
-//     const ran = 512314123
-//     return ran
-// }
 
 export default token;
